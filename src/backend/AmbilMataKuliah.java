0bfc4720acd8b85fefed4ac0c5d07fe830344077
wehgus <50318345+wehgus@users.noreply.github.com>
2019-05-07 18:34:04 +0700

    nambah convert

--- src/backend/AmbilMataKuliah.java
+++ src/backend/AmbilMataKuliah.java
@@ -12,7 +12,24 @@ public class AmbilMataKuliah {
         this.nilai = nilai;
     }
     
-    
+    public void convertnilai (){
+        double score;
+        if( nilai == "A"){
+            score = 4;
+        } else if( nilai == "AB" ){
+            score = 3.5;
+        } else if( nilai == "B" ){
+            score = 3;
+        } else if( nilai == "BC" ){
+            score = 2.5;
+        } else if( nilai == "C" ){
+            score = 2;
+        } else if( nilai == "D" ){
+            score = 1;        
+        } else if( nilai == "E" ){
+            score = 0;
+        }
+    }
     
     public void calculatePoint() {
         //this.logicPoint = (double)this.nilai + this.mataKuliah.getLogicPointRate();
